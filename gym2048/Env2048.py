400f1bdda41922dec6eda97a6d30ee473a949908
ZacharyChenpk <954617373@qq.com>
2021-12-08 07:49:24 -0500

    update soft env

--- gym2048/Env2048.py
+++ gym2048/Env2048.py
@@ -43,6 +43,46 @@ class Env2048(gym.Env):
     def render(self, mode='human'):
         return np.array(self.game.get_state()).reshape((4,4))
 
+class Env2048soft(gym.Env):
+
+    def __init__(self):
+        # self.init_state = init_state
+        # self.size = size 
+        self.game = Game2048()
+        self.isTraining = False
+        self.action_space = spaces.Box(low=0, high=1, shape=(4,))
+        self.observation_space = spaces.MultiDiscrete([18]*16)
+        # self.observation_space = spaces.Discrete(18,shape=(4,4))
+
+    def set_training(self, flag):
+        self.isTraining = flag
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, p):
+        if self.isTraining:
+            u = np.random.choice([0,1,2,3], p=p)
+        else:
+            u = np.argmax(p)
+        next_board, reward, done = self.game.step(u)
+        return self._get_obs(), reward, done, {}
+
+    def reset(self):
+        self.game.reset()
+        return self._get_obs()
+
+    def _get_obs(self):
+        self.board = self.game.get_state()
+        return np.log2(np.where(self.board>0, self.board, 1)).flatten()
+
+    def get_state(self):
+        return self._get_obs()
+
+    def render(self, mode='human'):
+        return np.array(self.game.get_state()).reshape((4,4))
+
 # gym.envs.register(
 #      id='Env2048',
 #      entry_point='gym.envs.classic_control:Env2048',
--- gym2048/__init__.py
+++ gym2048/__init__.py
@@ -4,3 +4,8 @@ register(
      entry_point='gym2048.Env2048:Env2048',
      max_episode_steps=5000,
 )
+register(
+     id='Env2048soft-v0',
+     entry_point='gym2048.Env2048:Env2048soft',
+     max_episode_steps=5000,
+)
